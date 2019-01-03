734bd114b930799fd8fea8acb0b0cef24469f4a9
Wallef Antoine <antoine.wallef@gmail.com>
2019-01-03 11:41:47 +0100

    updates

---
+++ src/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+namespace Trois\Bake;
+
+use Cake\Core\Configure;
+use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+
+class Plugin extends BasePlugin
+{
+  public function bootstrap(PluginApplicationInterface $app)
+  {
+    Configure::write('Bake.theme', 'Trois/Bake');
+    $app->addPlugin('friendsofcake/search');
+  }
+}
